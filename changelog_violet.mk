6d5a1a42bfd0e2a898d59603ad30c2a542284d3f
Drpto <dr.juniornoob@gmail.com>
2023-10-17 00:19:40 +0530

    violet: Update violet ota
    Signed-off-by: Drpto <dr.juniornoob@gmail.com>

---
+++ changelog_violet.mk
@@ -0,0 +1,9 @@
+Highlights & Device Specific Changes:
+Build type: Monthly
+Device: Redmi Note 7Pro (Violet)
+Device maintainer: Dr.Opto
+
+===== 18.10.2023 =====
+- Add Miui Camera by default
+- Enable OTA support
+- Some underhood changes
